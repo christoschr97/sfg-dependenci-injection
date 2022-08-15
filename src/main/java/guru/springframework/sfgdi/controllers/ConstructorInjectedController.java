b4dcb7308141a2a30ceff1ffc4dac3e742c5717d
christos christodoulou <christoschristodoulou3@gmail.com>
2022-08-15 14:06:09 +0300

    first commit

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 12/26/19.
+ */
+@Controller
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    //    public ConstructorInjectedController(GreetingService greetingService) { //because we set also a primarygreeting service we dont need to se tthe qualifier. It will be injected because it is the primary one and we use a Controller in this class
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
